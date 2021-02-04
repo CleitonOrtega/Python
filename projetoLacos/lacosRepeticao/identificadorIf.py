1aa4823775f4df266bc574ab6eabc407f2c66142
Cleiton Ortega <c.ortega200935@gmail.com>
2021-02-04 11:14:10 -0200

    Primeiros laços em Python

---
+++ projetoLacos/lacosRepeticao/identificadorIf.py
@@ -0,0 +1,6 @@
+nome=input("Digite seu nome: ")
+idade=int(input("Digite sua idade: "))
+if idade >= 18:
+    print("O ", nome, " tem ", idade ," anos e ele pode tirar habilitação!")
+else:
+    print("O ", nome, " tem ", idade , " anos e ele não pode tirar habilitação!")
