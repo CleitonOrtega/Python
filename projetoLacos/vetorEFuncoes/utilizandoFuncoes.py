ac54e5a19911d028ba036aa0814935e918f039c6
Cleiton Ortega <c.ortega200935@gmail.com>
2021-02-05 12:36:17 -0200

    Criando funções e utilizando em outro arquivo

---
+++ projetoLacos/vetorEFuncoes/utilizandoFuncoes.py
@@ -0,0 +1,19 @@
+from vetorEFuncoes.criandoFuncoes import *
+
+minhaLista=[]
+print("\nPreenchendo...")
+preencherInventario(minhaLista)
+print("\nExibindo...")
+exibirInventario(minhaLista)
+
+print("\nPesquisando...")
+localizarPorNome(minhaLista)
+print("\nAlterando...")
+depreciarPorNome(minhaLista,20)
+
+print("\nExcluindo...")
+print(excluirPorSerial(minhaLista))
+exibirInventario(minhaLista)
+
+print("\nResumindo...")
+resumirValores(minhaLista)
